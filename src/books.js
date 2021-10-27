ff5e669549a46af8aab4f48985da1606036a6514
AlexC47 <alex.csibi@gmail.com>
2021-10-27 14:01:57 +0300

    first

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/615YaAiA-ML._AC_SL1500_.jpg',
+    title: 'VR thingy 2',
+    author: 'sahgfolkdshglsk dsafd',
+  },
+  {
+    id: 2,
+    img: 'https://s13emagst.akamaized.net/products/32502/32501277/images/res_d5266fea2bd1d275a3928985e005cba0.jpg',
+    title: 'sonypony',
+    author: 'sahgfolkdshglsk dsafd',
+  },
+  {
+    id: 3,
+    img: 'https://s13emagst.akamaized.net/products/38526/38525846/images/res_eefa3c571b9801ec3623ee669f3014fa.jpg?width=720&height=720&hash=7E7889B566A429C22E9DB4D9A78FA64C',
+    title: 'preystation',
+    author: 'sdfsdagas',
+  },
+]
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+// CSS
+import './index.css'
+
+import { books } from './books'
+import Book from './Book'
+
+// setup vars
+
+function BookList() {
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>
+      })}
+    </section>
+  )
+}
+
+ReactDom.render(<BookList />, document.getElementById('root'))
